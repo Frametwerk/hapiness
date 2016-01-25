6c0a3af05ec6e4ea787a87fc64caeb6deba45fea
jessethach <jessethach@gmail.com>
2016-01-25 10:41:21 -0800

    created tests for routes

--- test/jedi_routes_test.js
+++ test/jedi_routes_test.js
@@ -3,5 +3,45 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const expect = chai.expect;
 process.env.MONGOLABL_URI = 'mongodb://localhost/jedis_app_test';
-const server = require(__dirname + '/../server');
 const request = chai.request;
+require(__dirname + '/../server.js');
+
+
+describe('The Jedis API', () => {
+
+  it('should be able to retrieve all jedis', (done) => {
+    request('localhost:3000')
+      .get('/jedis')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(Array.isArray(res.body)).to.eql(true);
+        done();
+      });
+  });
+
+  it('should create a jedi with a POST', (done) => {
+    request('localhost:3000')
+      .post('/jedis')
+      .send({name: 'test-jesse',lightsaberColor: 'purple'})
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.body.name).to.eql('test-jesse');
+        expect(res.body).to.have.property('_id');
+        done();
+      });
+  });
+
+  it('should find one specific jedi', (done) => {
+    request('localhost:3000')
+      .get('/jedis/65d17700-c21b-11e5-942c-85d3edd309fd')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.body.name).to.eql('test-jin');
+        done();
+      });
+  });
+
+});
